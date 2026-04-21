f91328f6b0540a23fb3abc42f3a2991f0d69d811
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2026-04-21 17:29:45 +0200

    Non-modifying algorithms

--- paralgo/Makefile.mk
+++ paralgo/Makefile.mk
@@ -1,5 +1,6 @@
 include $(PARALGO_STL_DIR)/Makefile.mk
 include $(PARALGO_EXEC_DIR)/Makefile.mk
+include $(PARALGO_NONMOD_DIR)/Makefile.mk
 include $(PARALGO_NUMERIC_DIR)/Makefile.mk
 
 $(FINAL_PARALGO_PDF): $(PARALGO_PARTS) $(CONFIG_PARTS) | $(FINAL_DIR)
